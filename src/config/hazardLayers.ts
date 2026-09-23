cca521b7a0478cdf808c7e5ac5c7c0f97f7b3153
Aaron <mcleana@hawaii.edu>
2026-09-23 13:39:36 -1000

    updated hazard file name

--- src/config/hazardLayers.ts
+++ src/config/hazardLayers.ts
@@ -479,15 +479,37 @@ export const HAZARD_LAYERS: HazardLayerConfig[] = [
     ],
   }, 
   {
+    id: "pcl_filtered",
+    name: "Previously Contaminated Land",
+    visible: false,
+    icon: "FaBarrel",
+    color: "#000000",
+    filePath: "Prev_contaminated_land_filtered.pmtiles",
+    popupConfig: {
+      titleField: "Previously Contaminated Land",
+      fields: [
+        { key: "city", label: "City" },
+        { key: "county", label: "County" },
+        { key: "island", label: "Island" },
+        { key: "site___fac", label: "Site/Facility name" },
+        { key: "site_addre", label: "Address" },
+        { key: "heer_asses", label: "Hazardous Waste Assessment" },
+        { key: "heer_respo", label: "Hazardous Waste Response" },
+        { key: "nature_of", label: "Nature of Contamination" },
+        { key: "nature_o_1", label: "Nature of Contamination 1" },
+      ],
+    },
+  },
+  {
     id: "pcl_landfill",
     name: "Landfills",
     menuPanel: "locations",
     visible: false,
     icon: "FaTrash",
     color: "#000000",
-    filePath: "pcl_landfills.pmtiles",
+    filePath: "Prev_contaminated_land_landfills.pmtiles",
     popupConfig: {
-      titleField: "",
+      titleField: "Landfills",
       fields: [
         { key: "city", label: "City" },
         { key: "county", label: "County" },
@@ -501,28 +523,7 @@ export const HAZARD_LAYERS: HazardLayerConfig[] = [
       ],
     },
   },
-  {
-    id: "pcl_filtered",
-    name: "Previously Contaminated Land",
-    visible: false,
-    icon: "FaBarrel",
-    color: "#000000",
-    filePath: "pcl_filtered.pmtiles",
-    popupConfig: {
-      titleField: "",
-      fields: [
-        { key: "city", label: "City" },
-        { key: "county", label: "County" },
-        { key: "island", label: "Island" },
-        { key: "site___fac", label: "Site/Facility name" },
-        { key: "site_addre", label: "Address" },
-        { key: "heer_asses", label: "Hazardous Waste Assessment" },
-        { key: "heer_respo", label: "Hazardous Waste Response" },
-        { key: "nature_of", label: "Nature of Contamination" },
-        { key: "nature_o_1 ", label: "Nature of Contamination 1" },
-      ],
-    },
-  },
+
   /*
   {
     id: "potent_econ_loss",
